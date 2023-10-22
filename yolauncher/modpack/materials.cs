dcc7f02d5f03904542fcb852ed93d6027acc202b
Mjaed <christophe@roblin.no>
2023-10-22 23:36:08 +0200

    added materials to wood cart

--- yolauncher/modpack/materials.cs
+++ yolauncher/modpack/materials.cs
@@ -16,6 +16,62 @@ singleton Material(flag_PVP_flag)
    diffuseMap[1] = "yolauncher/modpack/art/Textures/TextureLib/flag_nm.dds";
    translucentBlendOp = "None";
 };
+//------------- WoodCart ---------------->
+singleton Material(MapleBark_Diffuse_Cart)
+{
+   mapTo = "MapleBark_cart";
+   diffuseMap[0] = "art/models/3d/environment/trees/maple/textures/maplebark_diffuse.dds";
+   diffuseMap[1] = "art/models/3d/environment/trees/maple/textures/maplebark_normal.dds";
+};
+
+singleton Material(MapleTree_Cart) // клен
+{
+   mapTo = "MapleTree_cart";
+   diffuseMap[0] = "art/models/3d/environment/trees/maple/textures/mapletree_diffuse.dds";
+   diffuseMap[1] = "art/models/3d/environment/trees/maple/textures/mapletree_normal.dds";
+};
+
+singleton Material(OakTree_Cart) // дуб
+{
+   mapTo = "OakTree_Diffuse_cart";
+   diffuseMap[0] = "art/models/3d/environment/trees/oak/textures/oaktree_diffuse.dds";
+   diffuseMap[1] = "art/models/3d/environment/trees/oak/textures/oaktree_normal.dds";
+};
+
+singleton Material(OakBark01_Cart) // дуб ствол
+{
+   mapTo = "OakBark01_cart";
+   diffuseMap[0] = "art/models/3d/environment/trees/oak/textures/oakbark_diffuse.dds";
+   diffuseMap[1] = "art/models/3d/environment/trees/oak/textures/oakbark_normal.dds";
+};
+
+singleton Material(BirchTree_Cart) // береза
+{
+   mapTo = "BirchTree_cart";
+   diffuseMap[0] = "art/models/3d/environment/trees/birch/textures/birchtree_diffuse.dds";
+   diffuseMap[1] = "art/models/3d/environment/trees/birch/textures/birchtree_normal.dds";
+};
+
+singleton Material(BirchBark_Cart) // береза ствол
+{
+   mapTo = "BirchBark_cart";
+   diffuseMap[0] = "art/models/3d/environment/trees/birch/textures/birchbark_diffuse.dds";
+   diffuseMap[1] = "art/models/3d/environment/trees/birch/textures/birchbark_normal.dds";
+};
+
+singleton Material(PineTree_Cart) // сосна
+{
+   mapTo = "PineTree_cart";
+   diffuseMap[0] = "art/models/3d/environment/trees/pine/textures/pinetree_diffuse.dds";
+   diffuseMap[1] = "art/models/3d/environment/trees/pine/textures/pinetree_normal.dds";
+};
+
+singleton Material(PineBark_Cart) // сосна ствол
+{
+   mapTo = "PineBark_cart";
+   diffuseMap[0] = "art/models/3d/environment/trees/pine/textures/pinebark_diffuse.dds";
+   diffuseMap[1] = "art/models/3d/environment/trees/pine/textures/pinebark_normal.dds";
+};
 //------------- Church ---------------->
 
 singleton Material(BlueWall_A_mat)
