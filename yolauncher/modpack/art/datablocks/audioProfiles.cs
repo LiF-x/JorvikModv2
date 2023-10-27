d72328f6c50043054fed1dfe0fce1f667bf705ee
WARPED IBUN <84038557+matty5350@users.noreply.github.com>
2023-10-28 00:31:12 +0100

    Church lights & Bell Tower Fix
    Bell Tower Sound Fix
    Church Lights/Torches enabled

--- yolauncher/modpack/art/datablocks/audioProfiles.cs
+++ yolauncher/modpack/art/datablocks/audioProfiles.cs
@@ -1,24 +1,17 @@
 	
 // Jorvik Mod
+datablock SFXProfile(BellTowerSound)
+{
+   local = 1;
+   filename = "yolauncher/modpack/art/sound/SFX/env_bell.ogg";
+   //description = AudioObjectsCloseLoop3D;
+   description = AudioDefault3D;
+};
+
 datablock SFXProfile(env_bell)
 {
    local = 1;
-   filename = "art/sound/SFX/env_bell.ogg";
-   description = AudioObjectsCloseLoop3D;
-};
-datablock SFXProfile(env_bell)
-{
-   filename = "art/sound/SFX/env_bell.ogg";
+   filename = "yolauncher/modpack/art/sound/SFX/env_bell.ogg";
+   //description = AudioObjectsCloseLoop3D;
    description = AudioDefault3D;
-   referenceDistance = 10;
-   maxDistance = 100;
-   preload = true;
-};
-datablock AudioDescription(AudioDefault3D)
-{
-   volume = 1.0;
-   isLooping = false;
-   is3D = true;
-   ReferenceDistance = 10.0;
-   maxDistance = 100.0;
 };
